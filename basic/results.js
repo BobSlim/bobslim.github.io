878865dd93f46d41259980e80f19ca21445948fb
Robert Lim <robertslim42@gmail.com>
2024-09-15 18:54:35 +1000

    fix: updates some prose

--- basic/results.js
+++ basic/results.js
@@ -2,17 +2,17 @@ export default [
 	{
 		threshold: 90,
 		title: "Basically Asian.",
-		text: "Yeah, you hit pretty much every box somehow. I'm impressed. You fit every stereotype I could think of, which makes you the Base Asian. Every other asian is just a subset of you.",
+		text: "You are the asianest asian. I bow at your feet.",
 	},
 	{
 		threshold: 70,
 		title: "Mostly Asian.",
-		text: "You hit most of them. That means you fit the stereotype, and I probably won't be wrong if I called you an asian. Welcome to the club, kiddo, proud of you.",
+		text: "You fit a stereotype, congratulations. Welcome to the club.",
 	},
 	{
 		threshold: 50,
 		title: "Mostly not Asian.",
-		text: "You didn't answer yes to many of them, which means you don't fit this stereotype. Congratulations on not being basic, I guess. Woohoo?",
+		text: "You don't really fit this stereotype! Maybe find some other cliche to be in.",
 	},
 	{
 		threshold: 0,
